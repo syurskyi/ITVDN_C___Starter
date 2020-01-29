726a4f67af5ce770b32a12dcf51b50ec33d0bffa
Grey <sergejyurskyj@yahoo.com>
2020-01-28 23:22:42 -0800

    Commited 001_Introduction files

---
+++ 001_Introduction/001_FirstApp/cmake-build-debug/CMakeFiles/001_FirstApp.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/001_FirstApp.dir/main.cpp.obj"
+  "001_FirstApp.pdb"
+  "001_FirstApp.exe"
+  "001_FirstApp.exe.manifest"
+  "lib001_FirstApp.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/001_FirstApp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ 001_Introduction/002_Example/002_Example.cpp
@@ -0,0 +1,16 @@
+#include "stdafx.h"
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "I am developer!";
+	cout << endl;
+	cout << 2 + 2;
+
+	// Задержка
+	cin.get();
+    return 0;
+}
+
